831db195a4527c9fa3f8d99fe7c9adf1f19cf949
nareksahakyan <nareksahakyan@gmx.com>
2016-10-27 23:03:39 +0400

    initial

---
+++ showerthoughts/Models/showerthoughtsDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace showerthoughts.Models
+{
+    public class showerthoughtsDBContext:DbContext
+    {
+        public showerthoughtsDBContext():base("ShowerConnection")
+        {
+        }
+
+        public DbSet<thought> thoughts {get; set;}
+    }
+}
---
+++ showerthoughts/Models/thought.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace showerthoughts.Models
+{
+    public class thought
+    {
+        public int id { get; set; }
+        public string title { get; set; }      
+        public string bdText { get; set; }
+        public DateTime date { get; set; }
+        public int rate { get; set; }
+        
+        //autor
+        //subthought
+    }
+}
