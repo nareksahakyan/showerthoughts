91b02226583723a6b69a1e021e5a6c752152cf3b
nareksahakyan <nareksahakyan@gmx.com>
2016-10-31 00:02:03 +0400

    ckedit

---
+++ showerthoughts/Controllers/ThoughtController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using showerthoughts.Models;
+
+namespace showerthoughts.Controllers
+{
+    public class ThoughtController : Controller
+    {
+        showerthoughtsDBContext db = new showerthoughtsDBContext();
+
+        // GET: Thought
+        public ActionResult Index()
+        {
+            return View(db.thoughts.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(thought thought)
+        {
+            thought.rate = 0;
+
+            if (ModelState.IsValid)
+            {
+                db.thoughts.Add(thought);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult goToThought(int id)
+        {
+            thought thought = db.thoughts.Find(id);
+            return View(thought);
+        }
+
+    }
+}
