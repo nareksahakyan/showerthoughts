a64a6913e11b0d64a3bc1de55832630ee9a7fce2
nareksahakyan <nareksahakyan@gmx.com>
2017-02-06 23:09:43 +0400

    from home feb06

---
+++ showerthoughts/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using showerthoughts.Models;
+
+namespace showerthoughts.Controllers
+{
+    public class AdminController : Controller
+    {
+       public ApplicationDbContext db_getUsers = new ApplicationDbContext();
+        
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult getUsers()
+        {
+            return PartialView(db_getUsers.Users.ToList());
+        }
+    }
+}
